7be2b183aea118dc0cbe68a24308840f095c1caf
EC2 Default User <ec2-user@ip-172-31-2-155.ap-southeast-1.compute.internal>
2021-11-09 08:27:57 +0000

    This is my meshiterro!

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'cf795d4a86197286bf23bee04a87e000a40ed7217d2f39fc887ae01097bf5380edffb5eb82139b3db07290e5ae2cc141d940f7f757027079b9ace5603e035fc4'
